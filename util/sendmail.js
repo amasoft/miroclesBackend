3c02e9c44f0ab8e4a37d7d8a03cc21b1e49bffdb
amasoft <amadifaraday@gmail.com>
2023-07-11 10:59:54 +0100

    add email verifaction template

--- util/sendmail.js
+++ util/sendmail.js
@@ -17,36 +17,50 @@ export const sendmail = async (verifycode, email, name) => {
     //   "<h4>welcome </h4>" +
     //   `<p>below is the verification code  ${verifycode} </p>`,
     html: `
-    <p>To complete the email verification process, please follow the steps below:</p>
-    <ol>
-        <li>Open your preferred web browser and login to your email box.</li>
-        <li>Copy the code in the verification mail</li>
-        <li>enter the copied code in the confirmation page</li>
-        <li>Click submit</li>
-    </ol>
-  </section>
-    
-  <section>
-    <h1>Subject: Account Email Verification - Action Required</h1>
+    <section>
+        {/* MESSAGE FROM OUR SITE VERIFICATION PAGE UPON SUCCESSFUL SUBMITION OF THE ENROLLMENT FORM */}
+        <p>To complete the email verification process, please follow the steps below:</p>
+        <ol>
+            <li>Open your preferred web browser and login to your email box.</li>
+            <li>Copy the code in the verification mail</li>
+            <li>enter the copied code in the confirmation page</li>
+            <li>Click Verify</li>
+        </ol>
+      </section>
+        
+      <section>
+        {/* MESSAGE SENT TO THE USER'S EMAIL BOX FOR VERIFICATION*/}
+        <h1>Subject: Account Email Verification - Action Required</h1>
 
-    <h2>Dear ${name},</h2>  <br/>
+        <h2>Dear ${name},</h2>
 
-    <p>Welcome to our platform! We're excited to have you on board. Before we can activate your account and grant you full access, we need to verify your email address. </p> <br/>
-    
-    <p>To complete the email verification process, please enter the code below in the confirmation page: </p> <br/>
+        <p>Welcome to our platform! We're excited to have you on board. Before we can activate your account and grant you full access, we need to verify your email address. </p>
+        
+        <p>To complete the email verification process, please enter the code below in the confirmation page: </p>
 
-    <span>${verifycode}</span> <br/>
+        <span>${verifycode}</span>
 
-    <p>For security reasons, the verification link will only remain valid for the next 24 hours. Therefore, we urge you to seize this opportunity and validate your email address promptly. Should the link expire, you will need to initiate the verification process anew.</p> <br/>
+        <p>For security reasons, the verification link will only remain valid for the next 24 hours. Therefore, we urge you to seize this opportunity and validate your email address promptly. Should the link expire, you will need to initiate the verification process anew.</p>
 
-    <p>Please rest assured that we take your privacy seriously, and your email address will be handled with the utmost confidentiality. We employ rigorous security measures to ensure your personal information remains protected.</p> <br/>
+        <p>Please rest assured that we take your privacy seriously, and your email address will be handled with the utmost confidentiality. We employ rigorous security measures to ensure your personal information remains protected.</p>
 
-    <p>If you encounter any issues during the verification process or have any questions, please don't hesitate to contact our support team at <span>support@miroclesconsolidation.com</span>. We'll be happy to assist you. </p>
+        <p>If you encounter any issues during the verification process or have any questions, please don't hesitate to contact our support team at <span>support@miroclesconsolidation.com</span>. We'll be happy to assist you. </p>
 
-    <p>Thank you for choosing our platform. We look forward to providing you with an exceptional experience! </p> <br/>
+        <p>Thank you for choosing our platform. We look forward to providing you with an exceptional experience! </p><br/>
 
-    <p>Best regards, <br/>Mirocles Academy Team </p>
-    `,
+        <p>Best regards, <br/>Mirocles Academy Team </p>
+      </section>
+        
+
+        {/* MESSAGE ON THE CONFIRMATION PAGE */}
+
+        {/* when successful */}
+        <p>Congratulations! Your account is now active, and you can start exploring our platforms.</p>
+        <a href='https://join.slack.com/t/miroclesacademy/shared_invite/zt-1ybtyk5tx-w6RPFKR9rF1nlN2VINEGMQ' rel='noreferrer' target='_blank' >Click here to join our student platform on slack</a>
+
+        {/* when a wrong code is entered */}
+        <p>You have entered an invalid code.</p>
+           `,
   };
 
   const sendMail = transporter.sendMail(mailOptions, function (error, info) {
